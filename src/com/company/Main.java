37f2c05e637d4e32fcc08c025e19c10266b5af2d
Yuri <yurishchavlev@gmail.com>
2018-08-28 20:22:08 +0300

    For-loop with 10 chances, exit and number of chances

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,19 +10,24 @@ public class Main {
     public static void main(String[] args) {
 
         int mynum = rand.nextInt(100) + 1;
-        System.out.println("My number is: " + mynum);
+        System.out.println("PC-number is: " + mynum);
 
-        int usernum = scan.nextInt();
-        //  System.out.println("User has entered: "+usernum);
+        for (int i = 1; i < 11; i++) {
 
-        if (mynum > usernum) {
-            System.out.println("PC-value is greater than user-value");
-        } else if (mynum < usernum) {
-            System.out.println("PC-value is smaller than user-value");
-        } else {
-            System.out.println("PC-value and user-value are equal");
+            int usernum = scan.nextInt();
+            //  System.out.println("User has entered: "+usernum);
+
+            if (mynum > usernum) {
+                System.out.println("PC-value is greater than user-value");
+
+            } else if (mynum < usernum) {
+                System.out.println("PC-value is smaller than user-value");
+            } else {
+                System.out.println("PC-value and user-value are equal");
+                System.out.println("The number of chanses used: "+i);
+                i=10;
+            }
         }
-
 
     }
 }
